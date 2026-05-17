c22acb0c5a180d34bc1c0122646c4a704e51d3db
nornsong <ahg077126@gmail.com>
2026-05-18 00:03:09 +0900

    .

---
+++ src/main/java/com/koreanwordle/game/domain/word.java
@@ -0,0 +1,30 @@
+package com.koreanwordle.game.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class word {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String word;
+
+    @Column
+    private String definition;
+
+    @Column // 품사
+    private String pos;
+
+//    public word(String word, String definition, String pos) {
+//        this.word = word;
+//        this.definition = definition;
+//        this.pos = pos;
+//    }
+}
